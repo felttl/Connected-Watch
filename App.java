ae53c553200a654e40abf44f786e42c07c0c3f93
AdrienSmbr <127119096+AdrienSmbr@users.noreply.github.com>
2025-01-08 14:49:30 +0100

    Add App.java

---
+++ App.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+/**
+ *
+ * @author florent
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double FC;
+        int Temp;
+
+        for (int i=0; i<10;i++) {
+            
+            FC = Math.random() * 180;
+            Temp = (int)(Math.random() * 40);
+         }
+    }
+}
