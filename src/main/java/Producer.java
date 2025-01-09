ac11d87535e79e4d028b08738cdaefb85024b920
AdrienSmbr <127119096+AdrienSmbr@users.noreply.github.com>
2025-01-09 17:35:53 +0100

    add

--- src/main/java/Producer.java
+++ src/main/java/Producer.java
@@ -21,12 +21,12 @@ public class Producer {
 
 				Watch watch = new Watch();
 
-				double EC = watch.getHeartRate(); 
+				double HR = watch.getHeartRate(); 
 				double Temp = watch.getTemp();
 				String id = watch.getId();
 
 				// Construction du message JSON
-				String message = String.format("{\"ID\": \"%s\", \"EC\": %.2f, \"Temp\": %.2f}",id, EC, Temp);
+				String message = String.format("{\"ID\": \"%s\", \"HR\": %.2f, \"Temp\": %.2f}",id, HR, Temp);
 
 				// Publication du message
 				channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
